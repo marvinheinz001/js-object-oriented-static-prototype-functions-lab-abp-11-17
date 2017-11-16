ea474b6cbdc19173f5982fd9eeb7b1e8c364fb4e
Max han <mwellh84@gmail.com>
2017-11-16 11:07:44 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,12 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+  // let icebreakers = []
+  // emails.forEach(function(email){
+  //   icebreakers.push( new IcebreakerResponse(email))
+  // })
+  return emails.map(function(email){
+    return new IcebreakerResponse(email)
+  })
+}
