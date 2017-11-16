5fa7a136b30100ac122164e41f61bd42172bb4f2
Max han <mwellh84@gmail.com>
2017-11-16 11:03:27 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,10 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-User.BatchCreate = function(data){
-  return data.map(function(userData){
-    return new User(userData[0], userData[1])
+Icebreaker.BatchCreate = function(emails){
+  let returns = []
+  emails.forEach(function(emails){
+    returns.push( new IcebreakerResponse(email))
   })
+  return icebreakers 
+}
